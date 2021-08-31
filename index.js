d1565085b9deb34cdb51444759cf0c5f56cde02f
abhishekverma26 <86958785+abhishekverma26@users.noreply.github.com>
2021-08-31 17:31:28 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const port2 = process.env.PORT2 || 3001;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello, I am abhishek\n'
+  const msg = 'Hello, From Node'
   res.end(msg);
 });
 
 const server2 = http.createServer((req, res) => {
 	res.statusCode = 200;
-	const msg2 = 'Hello again from abhishek\n'
+	const msg2 = 'Hello again from Node\n'
 	res.end(msg2);
 });
 
